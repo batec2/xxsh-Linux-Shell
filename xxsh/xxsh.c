47c96d901922e116c766867056012ec8e3743f43
Crush Bate <50185023+batec2@users.noreply.github.com>
2023-09-21 20:35:04 -0600

    Sync with desktop

--- xxsh/xxsh.c
+++ xxsh/xxsh.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include <String.h>
+#include <string.h>
 #include <stdlib.h>
 #include "xxsh.h"
 
 #define MAX_LENGTH 256
 
 int main(void){
+    mainLoop();
     return 0;
 }
 
 void mainLoop(){
     char buffer[MAX_LENGTH];
     if(fgets(buffer,MAX_LENGTH,stdin)!=NULL){
-        printf(buffer);
+        puts(buffer);
     }
 }
 
 char *parse(char *input){
-    
+    return input;    
 }
