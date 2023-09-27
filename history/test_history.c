ae24c33af44dc8cc09b4bfa874815231aede9d99
Dakota Doolaege <dakotadoolaege@gmail.com>
2023-09-27 11:05:35 -0600

    Fix gitignore and makefiles

---
+++ history/test_history.c
@@ -0,0 +1,20 @@
+/* Test file for history module */
+
+#include "history.h"
+
+int main()
+{
+    printf("Startings history module tests\n\n");
+
+    /*Test adding items to history. This is a manual test*/
+    printf("Adding three items to history\n\n");
+    add_history("ls -l");
+    add_history("pwd");
+    add_history("ps -ax | grep systemd");
+
+    /* Test printing out the history contents. This is a manual test */
+    printf("Printing history contents\n");
+    history();
+
+    printf("Testing complete for history module\n\n");
+}
