06afa2644e59a50c2757c8258ba4ddfa52e6ec69
Dakota Doolaege <dakotadoolaege@gmail.com>
2023-10-18 19:45:52 -0600

    Clean up binary module

--- binary/test_binary.c
+++ binary/test_binary.c
@@ -17,18 +17,23 @@ int main()
 	test(get_program("ls", &path) == 1, "searching path for program");
 	free(path);
 
-	char *arg_array[] = { "ls", "-a", "-l", "/usr/bin", NULL };
+	char *arg_array[] = { "ls", "-a", "-l", "/home", NULL };
 
 	test(run_background(arg_array) == 0, "checking for foreground command");
 
-	test(run_cmd(arg_array) == 0, "running a foreground ls command");
+	test(run_cmd(arg_array) == 1, "running a foreground ls command");
 
-	char *arg_array2[] = { "ls", "&", NULL };
+	char *tmp = malloc(2);
+	strcpy(tmp, "&");
+	char *arg_array2[] = { "ls", tmp, NULL };
 	test(run_background(arg_array2) == 1,
 	     "checking for background command");
+	printf("here!!!\n");
 
-	char *arg_array3[] = { "xmessage", "Hello, World", "&", NULL };
-	test(run_cmd(arg_array3) == 0,
+	tmp = malloc(2);
+	strcpy(tmp, "&");
+	char *arg_array3[] = { "xmessage", "Hello, World", tmp, NULL };
+	test(run_cmd(arg_array3) == 1,
 	     "running a background xmessage command");;
 	printf("hello, I've returned before xmessage!\n");
 
