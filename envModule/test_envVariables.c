208b533c866801b341be3dda98e460cb54b13dd1
Crush Bate <50185023+batec2@users.noreply.github.com>
2023-11-22 10:59:37 -0700

    removed bracket

--- envModule/test_envVariables.c
+++ envModule/test_envVariables.c
@@ -7,7 +7,7 @@ int main(void)
 	read_env();
 	print_var();
 	set_var("Test", "Test");
-	printf("Should be: /usr/bin/->%s\n", get_env("PATH")));
+	printf("Should be: /usr/bin/->%s\n", get_env("PATH"));
 	printf("Should be: 0<%i\n", check_var("USER"));
 	printf("Should be: 0>%i\n", check_var("NotHere"));
 	destroy_env();
